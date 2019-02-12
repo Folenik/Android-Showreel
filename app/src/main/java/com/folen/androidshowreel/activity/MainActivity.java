2bfcc2128528c9c04aa2cd4872673ed152c86a58
Michał Rachuba <rachuba.michal@gmail.com>
2019-02-12 21:32:18 +0100

    Added MainActivity. Fixed gradle libraries issue. Added some basic resources setup.

---
+++ app/src/main/java/com/folen/androidshowreel/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.folen.androidshowreel.activity;
+
+import android.os.Bundle;
+
+import com.folen.androidshowreel.R;
+import com.folen.androidshowreel.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
