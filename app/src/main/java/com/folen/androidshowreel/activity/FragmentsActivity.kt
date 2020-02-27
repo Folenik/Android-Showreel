75bab578b52f47c138368028fdb3cbb7516d4b4d
Folenik <kamil.mosz12@gmail.com>
2020-02-27 14:09:06 +0100

    quick fix with layout names

--- app/src/main/java/com/folen/androidshowreel/activity/FragmentsActivity.kt
+++ app/src/main/java/com/folen/androidshowreel/activity/FragmentsActivity.kt
@@ -11,7 +11,7 @@ class FragmentsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
+        setContentView(R.layout.activity_fragments)
     }
 
     fun intent(context: Context): Intent {
