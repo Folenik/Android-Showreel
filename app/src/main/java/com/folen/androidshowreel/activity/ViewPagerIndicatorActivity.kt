e7dd964d59716e5134689730480e231f90d623c8
Folenik <kamil.mosz12@gmail.com>
2020-02-28 08:55:20 +0100

    Initialization

---
+++ app/src/main/java/com/folen/androidshowreel/activity/ViewPagerIndicatorActivity.kt
@@ -0,0 +1,19 @@
+package com.folen.androidshowreel.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.folen.androidshowreel.R
+
+class ViewPagerIndicatorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewpager_indicator)
+    }
+
+    fun intent(context: Context): Intent {
+        return Intent(context, ViewPagerIndicatorActivity::class.java)
+    }
+
+}
