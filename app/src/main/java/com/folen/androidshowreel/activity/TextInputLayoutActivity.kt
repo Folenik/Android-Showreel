3852702e520bba9c4a6c52b9e02f5e80827b9dc9
Folenik <kamil.mosz12@gmail.com>
2020-03-03 16:59:54 +0100

    TextInputLayoutActivity finished.

--- app/src/main/java/com/folen/androidshowreel/activity/TextInputLayoutActivity.kt
+++ app/src/main/java/com/folen/androidshowreel/activity/TextInputLayoutActivity.kt
@@ -3,9 +3,11 @@ package com.folen.androidshowreel.activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.appcompat.app.AppCompatActivity
 import com.folen.androidshowreel.R
-
 
 class TextInputLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,16 @@ class TextInputLayoutActivity : AppCompatActivity() {
 
     fun intent(context: Context): Intent {
         return Intent(context, TextInputLayoutActivity::class.java)
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun loginAccount(view: View) {
+        Toast.makeText(applicationContext, getString(R.string.click), LENGTH_SHORT).show()
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun createAccount(view: View) {
+        Toast.makeText(applicationContext, getString(R.string.click), LENGTH_SHORT).show()
     }
 
 }
