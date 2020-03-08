2f9238c9a7bf7186e965a9535ea5cbe8effcb21f
Folenik <kamil.mosz12@gmail.com>
2020-03-08 20:33:01 +0100

    LocalNotificationsActivity init.

---
+++ app/src/main/java/com/folen/androidshowreel/activity/LocalNotificationsActivity.kt
@@ -0,0 +1,20 @@
+package com.folen.androidshowreel.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.folen.androidshowreel.R
+import com.folen.androidshowreel.model.KotlinDataClass
+import kotlinx.android.synthetic.main.activity_kotlin.*
+
+class LocalNotificationsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_local_notifications)
+    }
+
+    fun intent(context: Context): Intent {
+        return Intent(context, LocalNotificationsActivity::class.java)
+    }
+}
